d055c714fed0055c436381225fce363cb85849b5
chimpansiets <svoort1@live.nl>
2022-11-21 12:09:55 +0100

    start of routing setup

---
+++ src/scenes/global/SideBar.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+
+const SideBar = () => {
+	return (
+		<div>Sidebar</div>
+	)
+}
+
+export default SideBar;
